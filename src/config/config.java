40c5417d8efc1b40df79857bd43d3989f870d7b7
valsilud <valsilud@gmail.com>
2025-09-08 02:31:52 -0700

    test

---
+++ src/config/config.java
@@ -0,0 +1,21 @@
+
+package config;
+
+import java.sql.*;
+
+public class config {
+    
+  
+public static Connection connectDB() {
+        Connection con = null;
+        try {
+            Class.forName("org.sqlite.JDBC"); // Load the SQLite JDBC driver
+            con = DriverManager.getConnection("jdbc:sqlite:voters.db"); // Establish connection
+            System.out.println("Connection Successful");
+        } catch (Exception e) {
+            System.out.println("Connection Failed: " + e);
+        }
+        return con;
+    }
+    
+}
